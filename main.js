4f758bf748e661c5c254ca60e5f625db5288e2cd
wagnerlucas91 <wlucaas@gmail.com>
2025-04-04 20:45:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let botao = document.getElementById("btn")
+let tarefa = document.getElementById("input-tarefa")
+let lista = document.getElementById("list")
+
+botao.addEventListener('click', function(){
+    let paragrafo = document.createElement('p')
+    paragrafo.innerHTML = tarefa.value
+    paragrafo.classList.add("estilo-paragrafo")
+    lista.appendChild(paragrafo)
+
+    paragrafo.addEventListener('click', function() {
+       lista.removeChild(paragrafo)
+    })
+
+
+})
